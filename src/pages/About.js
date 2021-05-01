91ff20766a37fe36e412433cf8fddb6ae304faa0
AlexCrow <iamalexcrow@gmail.com>
2021-05-01 16:00:39 -0500

    final

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      hanks for the thorough and fair review! Garmin missed an opportunity with trail and ultra runners. I would've preferred to see maps, wifi and music included, but able to turn on and off (so download latest map, then go offline for a run) to preserve battery life. Or use live maps with ClimbPro if battery life is not an issue. And as an ultra runner, don't need 80 hours b/c can do 100 miler with my 945 and for multi-day event (200+ miles) can recharge overnight. Maybe this is something Garmin can address in an update.
+      </p>
+    </section>
+  )
+}
+
+export default About
